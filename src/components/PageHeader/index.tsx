a6586d2c1d686c8925cdcffb05e6dd2163560a5b
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-03 20:02:27 -0300

    Criado study form para mobile

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -8,7 +8,7 @@ interface PageHeaderProps {
   title: string;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
+const PageHeader: React.FC<PageHeaderProps> = ({title, children}) => {
   return (
     <header className="page-header">
       <div className="top-bar-container">
@@ -20,6 +20,8 @@ const PageHeader: React.FC<PageHeaderProps> = ({title}) => {
 
       <div className="header-content">
         <strong>{title}</strong>
+        
+        {children}
       </div>
     </header>
   );
