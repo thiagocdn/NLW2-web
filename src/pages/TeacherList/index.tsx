0fa32b67a3301b208519d60b5ef119bf4d3cc9d6
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-03 21:24:38 -0300

    Criado card de professor

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
@@ -24,6 +26,34 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img src="https://avatars3.githubusercontent.com/u/61380775?s=460&u=0327807cc2d153c7b849154fb8343036432559be&v=4" alt="Thiago Cedran"/>
+            <div>
+              <strong>Thiago Cedran</strong>
+              <span>Física</span>
+            </div>
+          </header>
+          <p>
+            Aulas de Física bem estruturadas e expositiva.
+            <br /> <br />
+            Muita prática e exercícios para gravar e aprender com maior eficiência!
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 50,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em Contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 };
