11d3e08ac4e064a498c9e135ccff0cca82a19d77
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-05 20:08:41 -0300

    criado component input

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css'
 
@@ -15,24 +18,28 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
-
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name" />
-          </div>
-          
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp" />
-          </div>
-
-
+          <Input name='name' label='Nome completo' />
+          <Input name='avatar' label='Avatar' />
+          <Input name='whatsapp' label='Whatsapp' />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name='subject' label='Matéria' />
+          <Input name='cost' label='Custo da sua hora/aula' />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante"/>
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+
       </main>
     </div>
   )
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 
 function TeacherList() {
@@ -10,20 +11,11 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Input name="subject" label="Matéria"/>
+          <Input name="week_day" label="Dia da Semana"/>
+          <Input type="time" name="time" label="Hora"/>
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
         </form>
       </PageHeader>
 
