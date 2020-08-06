fdd99e6a22ba6337e151a54ae9717289f30f7588
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-05 21:38:44 -0300

    Conectado à API

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import { Link } from 'react-router-dom';
@@ -8,8 +8,17 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing () {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      setTotalConnections(response.data.total);
+    })
+  },[]);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -36,7 +45,7 @@ function Landing () {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas.
+          Total de {totalConnections} conexões já realizadas.
           <img src={purpleHeartIcon} alt="Coração Roxo"/>
         </span>
       </div>
