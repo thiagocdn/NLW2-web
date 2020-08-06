18dc47e3a4ea3fa7f9af1af7588c6e52af64da4c
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-08-06 18:36:09 -0300

    criado conexão com aulas cadastradas

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,26 +4,38 @@ import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/61380775?s=460&u=0327807cc2d153c7b849154fb8343036432559be&v=4" alt="Thiago Cedran"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Thiago Cedran</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Aulas de Física bem estruturadas e expositiva.
-        <br /> <br />
-        Muita prática e exercícios para gravar e aprender com maior eficiência!
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 50,00</strong>
+          <strong>R$ {teacher.cost},00</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher }  from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
-
+import api from '../../services/api';
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeek_day] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers(event: FormEvent) {
+    event.preventDefault();
+
+    const response = await api.get('/classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      }
+    });
+    
+    setTeachers(response.data);
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis.">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
 
           <Select
             name='subject'
             label='Matéria'
+            value={subject}
+            onChange={(event) => {setSubject(event.target.value)}}
             options={[
               { value: 'Artes', label: 'Artes'},
               { value: 'Fisica', label: 'Fisica'},
@@ -29,6 +51,8 @@ function TeacherList() {
           <Select
             name='week_day'
             label='Dia da semana'
+            value={week_day}
+            onChange={(event) => {setWeek_day(event.target.value)}}
             options={[
               { value: '0', label: 'Domingo'},
               { value: '1', label: 'Segunda-feira'},
@@ -39,15 +63,25 @@ function TeacherList() {
               { value: '6', label: 'Sábado'},
             ]}
           />
-          <Input type="time" name="time" label="Hora"/>
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(event) => {setTime(event.target.value)}}
+          />
 
+          <button type="submit">
+            Buscar
+          </button>
+            
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem teacher={teacher} key={teacher.id} />
+        })}
       </main>
     </div>
   )
